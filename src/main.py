7c9009f6350d1d8747c6b47cdab33ab8d96c34f2
Sam van der Kris <samvdkris@disroot.org>
2020-07-13 14:25:29 +0200

    Simple sanic+peewee demo

--- src/main.py
+++ src/main.py
@@ -1,39 +1,27 @@
 from sanic import Sanic, response
+import models
 import os
 import json
 
 app = Sanic('Test API')
 
-DATABASE_FILE = 'database.json'
 
+@app.route('/store/<name>')
+async def r_store(req, name):
+	p = models.Person(name=name)
+	p.save()
+	return response.text('Person added')
 
-def load_db():
-	with open(DATABASE_FILE) as f:
-		return json.load(f)
-
-
-@app.route('/store', methods=['POST'])
-async def r_store(req):
-	name = req.json['name']
-	webid = req.json['webid']
-
-	db = load_db()
-	db.append({
-		'name': name,
-		'webid': webid
-	})
-	with open(DATABASE_FILE, 'w') as f:
-		json.dump(db, f)
-
-	return response.empty()
 
 
 @app.route('/get')
 async def r_get(req):
-	db = load_db()
-	return response.json(db)
+	people = models.Person.select()
+	res = [(p.id, p.name) for p in people]
+	return response.json(res)
 
 
 
 if __name__ == '__main__':
+	models.db.create_tables([models.Person])
 	app.run(host='0.0.0.0', port=8000, debug=os.environ.get('DEBUG'))
