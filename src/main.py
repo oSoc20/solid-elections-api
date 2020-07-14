6fd736c5d33eeaa36c24ce5980827feeaec707c3
JonasVervloet <vervloetjonas@hotmail.com>
2020-07-14 11:36:31 +0200

    added name filtering
    webids are filtered based on the name given

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from sanic import Sanic, response
 from playhouse.shortcuts import model_to_dict
 import models
+from rdflib import Graph, RDF
+from rdflib.namespace import FOAF
 import os
 import json
 
@@ -18,13 +20,52 @@ async def r_store(req):
 
 @app.route('/get')
 async def r_get(req):
+	return response.json(get_web_ids())
+
+
+
+@app.route('/get/<name>')
+async def r_get(req, name):
+	web_ids = get_web_ids()
+
+	valid_webids = []
+	for web_id in web_ids:
+		uri = web_id['uri']
+		graph = Graph()
+		graph.parse(uri)
+
+		web_id_added = False
+		# get all Persons in the solid pod and loop over them
+		for person in graph.subjects(RDF.type, FOAF.Person):
+			# get all names of these Persons and loop over them
+			for webid_name in graph.objects(person, FOAF.name):
+				# check if the name on the solid pod equals the given name
+				if check_equal_names(name, str(webid_name)):
+					valid_webids.append(web_id)
+					web_id_added = True
+					break
+
+			# make sure the web id is only added once to the list
+			if web_id_added:
+				break
+
+	return response.json(valid_webids)
+
+
+def get_web_ids():
 	webids = models.WebID.select()
 
 	webids = [model_to_dict(wid) for wid in webids]  # Convert list of ModelSelect objects to Python dicts
 	for wid in webids:
 		wid['date_created'] = wid['date_created'].isoformat()  # Convert Python datetime object to ISO 8601 string
 
-	return response.json(webids)
+	return webids
+
+
+
+def check_equal_names(name1, name2):
+	# TODO: check for small typo's?
+	return name1 == name2
 
 
 
